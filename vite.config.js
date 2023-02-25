e77341d8abd641927f4e68908a5a02f6bed43cfe
Valentin Galfre <89883616+galfrevn@users.noreply.github.com>
2023-02-25 12:44:30 -0300

    fix: Adding Vite configuration params to avoid memory leaks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    minify: false,
+    sourcemap: false,
+    rollupOptions: {
+      cache: false
+    }
+  },
+});
